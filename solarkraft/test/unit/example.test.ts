b0bd043924813e35531749db7baa1029e3031820
Igor Konnov <igor@konnov.phd>
2024-04-15 17:01:49 +0200

    add an example unit test

---
+++ solarkraft/test/unit/example.test.ts
@@ -0,0 +1,10 @@
+// an example unit test to copy from
+
+import { assert } from 'chai'
+import { describe, it } from 'mocha'
+
+describe('example test', () => {
+    it('asserts', () => {
+        assert.equal(4, 2 + 2)
+    })
+})
