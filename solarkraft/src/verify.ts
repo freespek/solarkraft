6dee786b095ca18806004bfc931932417e2615fe
Thomas Pani <thomas.pani@gmail.com>
2024-04-19 10:48:14 +0000

    Launch Apalache on `verify`

--- solarkraft/src/verify.ts
+++ solarkraft/src/verify.ts
@@ -6,10 +6,47 @@
  * Verify transactions fetched from the ledger
  * @param args the arguments parsed by yargs
  */
+
+import { spawnSync } from 'child_process'
+import { existsSync } from 'node:fs'
+import path from 'node:path'
+
+// TODO: fix hardcoded path to Apalache
+const APALACHE_DIST = '/opt/apalache'
+const APALACHE_BIN = path.join(APALACHE_DIST, 'bin', 'apalache-mc')
+
 export function verify(args: any) {
     console.log(
         `*** WARNING: THIS IS A MOCK. NOTHING IS IMPLEMENTED YET. ***\n`
     )
     console.log(`Verifying transaction: ${args.txHash}...`)
-    console.log(`[OK]`)
+
+    if (!existsSync(args.monitor)) {
+        console.log(`The monitor file ${args.monitor} does not exist.`)
+        console.log('[Error]')
+        return
+    }
+
+    console.log(`Running Apalache (check ${args.monitor})...`)
+    const child = spawnSync(APALACHE_BIN, ['check', args.monitor])
+
+    switch (child.status) {
+        case 0: {
+            console.log('[OK]')
+            break
+        }
+        case 12: {
+            // Deadlock
+            console.log('Monitor is unable to reproduce the transaction')
+            console.log('[Fail]')
+            break
+        }
+        default: {
+            console.log(
+                `Internal error: Apalache failed with error code ${child.status}`
+            )
+            console.log('[Error]')
+            break
+        }
+    }
 }
