6d3a8154ac44639bcd13fb77b04cd0e7abd58633
Thomas Pani <thomas.pani@gmail.com>
2024-05-06 09:24:56 +0000

    Add e2e test

--- solarkraft/test/e2e/verify.test.ts
+++ solarkraft/test/e2e/verify.test.ts
@@ -6,7 +6,9 @@ import { spawn } from 'nexpect'
 
 describe('verify', () => {
     it('fails on missing file', function (done) {
-        spawn('solarkraft verify --txHash mimimi --monitor doesnotexist')
+        spawn(
+            'solarkraft verify --txHash mimimi --monitor doesnotexist --state test/e2e/tla/timelock_state.itf.json'
+        )
             .wait('The monitor file doesnotexist does not exist.')
             .expect('[Error]')
             .run(done)
@@ -15,7 +17,7 @@ describe('verify', () => {
     it('reports success on okay monitor', function (done) {
         this.timeout(50000)
         spawn(
-            'solarkraft verify --txHash mimimi --monitor test/e2e/tla/timelock_mon1_instrumented_okay.tla'
+            'solarkraft verify --txHash mimimi --monitor test/e2e/tla/timelock_mon1_instrumented_okay.tla --state test/e2e/tla/timelock_state.itf.json'
         )
             .wait('[OK]')
             .run(done)
@@ -24,7 +26,7 @@ describe('verify', () => {
     it('reports failure on deadlocking monitor', function (done) {
         this.timeout(50000)
         spawn(
-            'solarkraft verify --txHash mimimi --monitor test/e2e/tla/timelock_mon1_instrumented_fail.tla'
+            'solarkraft verify --txHash mimimi --monitor test/e2e/tla/timelock_mon1_instrumented_fail.tla --state test/e2e/tla/timelock_state.itf.json'
         )
             .wait('Monitor is unable to reproduce the transaction')
             .expect('[Fail]')
