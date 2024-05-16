865e850f23972e027df86c40cb5bca56f7ec5649
Igor Konnov <igor@konnov.phd>
2024-05-16 15:34:59 +0200

    add a simple implementation of ADR002

--- solarkraft/src/fetcher/storage.ts
+++ solarkraft/src/fetcher/storage.ts
@@ -9,7 +9,12 @@
  * Igor Konnov, 2024.
  */
 
+import JSONbigint from 'json-bigint'
 import { OrderedMap } from 'immutable'
+import { join } from 'node:path/posix'
+import { mkdirSync, readFileSync, writeFileSync } from 'node:fs'
+
+const JSONbig = JSONbigint({ useNativeBigInt: true })
 
 /**
  * Ordered mapping from field names to their native values (JS),
@@ -61,4 +66,69 @@ export interface ContractCallEntry {
      * when the storage goes over TTL.
      */
     oldFields: FieldsMap
+}
+
+/**
+ * Store a contract call entry in the file storage.
+ * @param root the storage root directory
+ * @param entry a call entry
+ */
+export function saveContractCallEntry(root: string, entry: ContractCallEntry) {
+    const filename = getEntryFilename(root, entry)
+    // convert OrderedMaps to arrays
+    const simplified = {
+        ...entry,
+        fields: entry.fields.toArray(),
+        oldFields: entry.oldFields.toArray(),
+    }
+    const contents = JSONbig.stringify(simplified)
+    writeFileSync(filename, contents)
+    return filename
+}
+
+/**
+ * Load a contract call entry in the file storage.
+ * @param root the storage root directory
+ * @param entry a call entry
+ * @returns the loaded call entry
+ */
+export function loadContractCallEntry(filename: string): ContractCallEntry {
+    const contents = readFileSync(filename)
+    const loaded = JSONbig.parse(contents)
+    return {
+        height: loaded.height as number,
+        contractId: loaded.contractId as string,
+        txHash: loaded.txHash as string,
+        method: loaded.method as string,
+        methodArgs: loaded.methodArgs as any[],
+        returnValue: loaded.returnValue,
+        fields: OrderedMap<string, any>(loaded.fields),
+        oldFields: OrderedMap<string, any>(loaded.oldFields),
+    }
+}
+
+/**
+ * Get the filename for a contract call entry. Create the parent directory, if required.
+ *
+ * @param root storage root
+ * @param entry call entry
+ * @returns the filename
+ */
+function getEntryFilename(root: string, entry: ContractCallEntry) {
+    const dir = getOrCreateDirectory(root, entry)
+    return join(dir, `entry-${entry.txHash}.json`)
+}
+
+/**
+ * Return the parent directory for an entry.
+ * If this directory does not exist, create it recursively.
+ *
+ * @param root storage root
+ * @param entry call entry
+ * @returns the directory name
+ */
+function getOrCreateDirectory(root: string, entry: ContractCallEntry) {
+    const directory = join(root, entry.contractId, entry.height.toString())
+    mkdirSync(directory, { recursive: true })
+    return directory
 }
