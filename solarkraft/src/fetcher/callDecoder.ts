4123348200c29e8f7ae538a3782dc1ceef5da828
Igor Konnov <igor@konnov.phd>
2024-05-16 13:44:01 +0200

    simplify branching

--- solarkraft/src/fetcher/callDecoder.ts
+++ solarkraft/src/fetcher/callDecoder.ts
@@ -24,95 +24,86 @@ export async function extractContractCall(
     matcher: (contractId: string) => boolean
 ): Promise<Maybe<ContractCallEntry>> {
     // https://developers.stellar.org/network/horizon/api-reference/resources/operations/object
-    if (op.function === 'HostFunctionTypeHostFunctionTypeInvokeContract') {
-        // This operation represents a smart contract call.
-        // In particular, it contains: the callee contract, the method, and its parameters.
-        //
-        // https://developers.stellar.org/network/horizon/api-reference/resources/operations/object/invoke-host-function
-
-        // extract the call data: transaction hash, parameters
-        const txHash = op.transaction_hash
-        // decode the call parameters from XDR to native JS values
-        const params = op.parameters.map((p) => {
-            return sdk.scValToNative(sdk.xdr.ScVal.fromXDR(p.value, 'base64'))
-        })
-        const contractId = params[0]
-        if (!matcher(contractId)) {
-            // return quickly to avoid additional calls and deserialization
-            return none()
-        }
-
-        // continue with extracting transaction data and ledger updates
-        const method = params[1]
-        const methodArgs = params.slice(2)
-
-        const tx = await op.transaction()
-        // is it the right way to extract the height?
-        const height = tx.ledger_attr
-
-        // The operation itself does not give us the ledger updates.
-        // Deserialize tx.result_meta_xdr.
-        const meta = sdk.xdr.TransactionMeta.fromXDR(
-            tx.result_meta_xdr,
-            'base64'
-        )
-        // extract transaction metadata for version 3
-        if (meta.switch() !== 3) {
-            // Is it possible to have older transaction metadata with Soroban?
-            // TODO: make it future-proof
-            console.error(
-                `Expected TransactionMetaV3, found ${meta.switch()}: txHash = ${txHash}`
-            )
-            return none()
-        } else {
-            const meta3 = meta.value() as sdk.xdr.TransactionMetaV3
-            const fieldsUpdates: FieldsMap[] = meta3
-                .operations()
-                .map((o) =>
-                    o
-                        .changes()
-                        .map(ledgerEntry(contractId))
-                        .filter((c) => c.isJust())
-                        .map((c) => c.value)
-                )
-                .flat()
-
-            if (fieldsUpdates.length > 2) {
-                console.error(
-                    `Transaction ${txHash} has over two contract states`
-                )
-            }
-
-            // contract storage after the operations were applied
-            const fields =
-                fieldsUpdates.length >= 1
-                    ? fieldsUpdates.pop()
-                    : OrderedMap<string, any>()
-            // contract storage before the operations were applied
-            const oldFields =
-                fieldsUpdates.length >= 1
-                    ? fieldsUpdates.pop()
-                    : OrderedMap<string, any>()
-
-            // decode return value
-            const returnValue = sdk.scValToNative(
-                meta3.sorobanMeta().returnValue()
-            )
-
-            return just({
-                height,
-                txHash,
-                contractId,
-                method,
-                methodArgs,
-                returnValue,
-                fields,
-                oldFields,
-            })
-        }
+    if (op.function !== 'HostFunctionTypeHostFunctionTypeInvokeContract') {
+        return none()
     }
 
-    return none()
+    // This operation represents a smart contract call.
+    // In particular, it contains: the callee contract, the method, and its parameters.
+    //
+    // https://developers.stellar.org/network/horizon/api-reference/resources/operations/object/invoke-host-function
+
+    // extract the call data: transaction hash, parameters
+    const txHash = op.transaction_hash
+    // decode the call parameters from XDR to native JS values
+    const params = op.parameters.map((p) => {
+        return sdk.scValToNative(sdk.xdr.ScVal.fromXDR(p.value, 'base64'))
+    })
+    const contractId = params[0]
+    if (!matcher(contractId)) {
+        // return quickly to avoid additional calls and deserialization
+        return none()
+    }
+
+    // continue with extracting transaction data and ledger updates
+    const method = params[1]
+    const methodArgs = params.slice(2)
+
+    const tx = await op.transaction()
+    // is it the right way to extract the height?
+    const height = tx.ledger_attr
+
+    // The operation itself does not give us the ledger updates.
+    // Deserialize tx.result_meta_xdr.
+    const meta = sdk.xdr.TransactionMeta.fromXDR(
+        tx.result_meta_xdr,
+        'base64'
+    )
+    // extract transaction metadata for version 3
+    if (meta.switch() !== 3) {
+        // Is it possible to have older transaction metadata with Soroban?
+        // TODO: make it future-proof
+        console.error(
+            `Expected TransactionMetaV3, found ${meta.switch()}: txHash = ${txHash}`
+        )
+        return none()
+    }
+
+    const meta3 = meta.value() as sdk.xdr.TransactionMetaV3
+    const fieldsUpdates: FieldsMap[] = meta3
+        .operations()
+        .map((o) =>
+            o
+                .changes()
+                .map(ledgerEntry(contractId))
+                .filter((c) => c.isJust())
+                .map((c) => c.value)
+        )
+        .flat()
+
+    if (fieldsUpdates.length > 2) {
+        console.error(
+            `Transaction ${txHash} has over two contract states`
+        )
+    }
+
+    // contract storage after the operations were applied
+    const fields =
+        fieldsUpdates.length >= 1
+            ? fieldsUpdates.pop()
+            : OrderedMap<string, any>()
+    // contract storage before the operations were applied
+    const oldFields =
+        fieldsUpdates.length >= 1
+            ? fieldsUpdates.pop()
+            : OrderedMap<string, any>()
+
+    // decode return value
+    const returnValue = sdk.scValToNative(
+        meta3.sorobanMeta().returnValue()
+    )
+
+    return just({ height, txHash, contractId, method, methodArgs, returnValue, fields, oldFields, })
 }
 
 // decode a contract entry that is changed by a contract call
