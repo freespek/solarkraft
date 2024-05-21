f12a2b1da819096c26dca6e5043c715b0d993402
Kukovec <jure.kukovec@gmail.com>
2024-05-21 15:22:58 +0200

    RM'd TTL extension

--- ContractExamples/contracts/alert/src/lib.rs
+++ ContractExamples/contracts/alert/src/lib.rs
@@ -36,12 +36,6 @@ impl Alert {
         alerts.set(tx_hash, status);
         env.storage().instance().set(&ALERTS, &alerts);
 
-        // The contract instance will be bumped to have a lifetime of at least 100 ledgers if the current expiration lifetime at most 50.
-        // If the lifetime is already more than 100 ledgers, this is a no-op. Otherwise,
-        // the lifetime is extended to 100 ledgers. This lifetime bump includes the contract
-        // instance itself and all entries in storage().instance().
-        env.storage().instance().extend_ttl(50, 100);
-
         // If there is a violation, emit an event.
         if status == MonitorAnalysisStatus::Violation {
             env.events().publish((ALERTS, VIOLATION), status);
