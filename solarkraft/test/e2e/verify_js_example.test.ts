932ce1076983fc2f6111d7e86d2a891007c6539b
Thomas Pani <thomas.pani@gmail.com>
2024-10-21 12:04:10 +0200

    Add a test for the Xycloans case study

--- solarkraft/test/e2e/verify_js_example.test.ts
+++ solarkraft/test/e2e/verify_js_example.test.ts
@@ -30,4 +30,28 @@ describe('verify JavaScript monitor', () => {
             .expect('[OK]: tx reverted as expected by reverts_if conditions')
             .run(done)
     })
+
+    it('reports error on `verify_js_example_xycloans.ts`', function (done) {
+        this.timeout(50_000)
+        spawn('node dist/test/e2e/verify_js_example_xycloans.js', {
+            stream: 'all',
+        })
+            .wait(
+                'Verifying deposit (successful tx 49ce70a0fe73f9b815f50dbe13403608242cec0770ac32eb24d1a425e39467be)...'
+            )
+            .expect('[OK]: all succeeds_with conditions hold')
+            .wait(
+                'Verifying deposit (successful tx af8d698431a4354bbe74517d741bbb278180eae2c6907ddc0abf5fcacd8b938b)...'
+            )
+            .expect('[OK]: all succeeds_with conditions hold')
+            .wait(
+                'Verifying borrow (successful tx 20a4726ab7291e00bfaad767a034844ffffd25f73882b5288da5486e3cc01973)...'
+            )
+            .expect('[OK]: all succeeds_with conditions hold')
+            .wait(
+                'Verifying update_fee_rewards (successful tx e5064617326554c02de8edd6628f1e1951903497698b3f1f5f0b00e03b2a16b6)...'
+            )
+            .expect('[Error]: some succeeds_with conditions do not hold')
+            .run(done)
+    })
 })
