2a1de66206458a9f6d41c83c88e6d618905d4b8f
Igor Konnov <igor@konnov.phd>
2024-11-29 19:54:28 +0100

    rename soroban to stellar

--- ContractExamples/scripts/setter-populate.sh
+++ ContractExamples/scripts/setter-populate.sh
@@ -15,53 +15,53 @@ dir=$(cd `dirname $0`; pwd -P)
 cd ${dir}/..
 
 NET=testnet
-(soroban network ls | grep -q $NET) || (echo "add testnet via soroban network"; exit 1)
+(stellar network ls | grep -q $NET) || (echo "add testnet via stellar network"; exit 1)
 
 ACCOUNT=alice
-soroban keys address $ACCOUNT || (echo "add the account $ACCOUNT via soroban keys generate"; exit 1)
+stellar keys address $ACCOUNT || (echo "add the account $ACCOUNT via stellar keys generate"; exit 1)
 
 ACCOUNT2=bob
-soroban keys address $ACCOUNT2 || (echo "add the account $ACCOUNT2 via soroban keys generate"; exit 1)
+stellar keys address $ACCOUNT2 || (echo "add the account $ACCOUNT2 via stellar keys generate"; exit 1)
 
 set -x
 
-soroban contract build
-soroban contract deploy --wasm target/wasm32-unknown-unknown/release/setter.wasm \
+stellar contract build
+stellar contract deploy --wasm target/wasm32-unknown-unknown/release/setter.wasm \
       --source $ACCOUNT --network $NET | tee >.setter.id
 
-soroban contract invoke --id $(cat .setter.id) --source $ACCOUNT --network $NET \
+stellar contract invoke --id $(cat .setter.id) --source $ACCOUNT --network $NET \
       -- set_bool --v true
-soroban contract invoke --id $(cat .setter.id) --source $ACCOUNT --network $NET \
+stellar contract invoke --id $(cat .setter.id) --source $ACCOUNT --network $NET \
       -- set_u32 --v 42
-soroban contract invoke --id $(cat .setter.id) --source $ACCOUNT --network $NET \
+stellar contract invoke --id $(cat .setter.id) --source $ACCOUNT --network $NET \
       -- set_i32 --v '-42'
-soroban contract invoke --id $(cat .setter.id) --source $ACCOUNT --network $NET \
+stellar contract invoke --id $(cat .setter.id) --source $ACCOUNT --network $NET \
       -- set_u64 --v 42
-soroban contract invoke --id $(cat .setter.id) --source $ACCOUNT --network $NET \
+stellar contract invoke --id $(cat .setter.id) --source $ACCOUNT --network $NET \
       -- set_i64 --v '-42'
-soroban contract invoke --id $(cat .setter.id) --source $ACCOUNT --network $NET \
+stellar contract invoke --id $(cat .setter.id) --source $ACCOUNT --network $NET \
       -- set_u128 --v 42
-soroban contract invoke --id $(cat .setter.id) --source $ACCOUNT --network $NET \
+stellar contract invoke --id $(cat .setter.id) --source $ACCOUNT --network $NET \
       -- set_i128 --v '-42'
-soroban contract invoke --id $(cat .setter.id) --source $ACCOUNT --network $NET \
+stellar contract invoke --id $(cat .setter.id) --source $ACCOUNT --network $NET \
       -- set_sym --v hello
-soroban contract invoke --id $(cat .setter.id) --source $ACCOUNT --network $NET \
+stellar contract invoke --id $(cat .setter.id) --source $ACCOUNT --network $NET \
       -- set_bytes --v beef
-soroban contract invoke --id $(cat .setter.id) --source $ACCOUNT --network $NET \
+stellar contract invoke --id $(cat .setter.id) --source $ACCOUNT --network $NET \
       -- set_bytes32 --v beef0123456789beef0123456789beef0123456789ab
-soroban contract invoke --id $(cat .setter.id) --source $ACCOUNT --network $NET \
+stellar contract invoke --id $(cat .setter.id) --source $ACCOUNT --network $NET \
       -- set_vec --v '[ 1, -2, 3 ]'
-soroban contract invoke --id $(cat .setter.id) --source $ACCOUNT --network $NET \
+stellar contract invoke --id $(cat .setter.id) --source $ACCOUNT --network $NET \
       -- set_map --v '{ "2": 3, "4": 5 }'
-soroban contract invoke --id $(cat .setter.id) --source $ACCOUNT --network $NET \
+stellar contract invoke --id $(cat .setter.id) --source $ACCOUNT --network $NET \
       -- set_address --v GDIY6AQQ75WMD4W46EYB7O6UYMHOCGQHLAQGQTKHDX4J2DYQCHVCR4W4
-soroban contract invoke --id $(cat .setter.id) --source $ACCOUNT --network $NET \
+stellar contract invoke --id $(cat .setter.id) --source $ACCOUNT --network $NET \
       -- set_struct --v '{ "a": 1, "b": "-100" }'
-soroban contract invoke --id $(cat .setter.id) --source $ACCOUNT --network $NET \
+stellar contract invoke --id $(cat .setter.id) --source $ACCOUNT --network $NET \
       -- set_enum --v '{ "B": "-200" }'
-soroban contract invoke --id $(cat .setter.id) --source $ACCOUNT --network $NET \
+stellar contract invoke --id $(cat .setter.id) --source $ACCOUNT --network $NET \
       -- remove_bool
 
 # we can provoke a failed transaction by submitting 2 transactions in parallel from different accounts
-soroban contract invoke --id $(cat .setter.id) --source $ACCOUNT --network $NET -- set_bool_if_notset &
-soroban contract invoke --id $(cat .setter.id) --source $ACCOUNT2 --network $NET -- set_bool_if_notset
+stellar contract invoke --id $(cat .setter.id) --source $ACCOUNT --network $NET -- set_bool_if_notset &
+stellar contract invoke --id $(cat .setter.id) --source $ACCOUNT2 --network $NET -- set_bool_if_notset
