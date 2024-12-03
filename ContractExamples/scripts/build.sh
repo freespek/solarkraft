475ecc59729bcf901d1fef748af6bc00d1cfea09
Igor Konnov <igor@konnov.phd>
2024-12-03 15:59:40 +0100

    pin xycloans and build it from build.sh

--- ContractExamples/scripts/build.sh
+++ ContractExamples/scripts/build.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
-# NOTE: we should use the standard command, but it does not seem to use the right flags
-# stellar contract build
+# see: https://github.com/stellar/rs-soroban-sdk/pull/1353
+rustc --version | grep 1.82.0 || (echo "Run: rustup default 1.82.0"; exit 1)
 
-RUSTFLAGS="-C target-feature=-reference-types" cargo build --target wasm32-unknown-unknown --release
+dir=$(cd `dirname $0`; pwd -P)
+cd ${dir}/..
+
+# build our contracts and soroban-examples
+stellar contract build
+
+# build xycloans
+cd xycloans
+stellar contract build
+
+# When using rust over 1.82.0:
+#RUSTFLAGS="-C target-feature=-reference-types" cargo build --target wasm32-unknown-unknown --release
