def3579b6697ef9d85bbd107ba4b2c3788ae7d6b
Igor Konnov <igor@konnov.phd>
2024-12-04 19:14:46 +0100

    fix a typo

--- ContractExamples/scripts/build.sh
+++ ContractExamples/scripts/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # see: https://github.com/stellar/rs-soroban-sdk/pull/1353
-rustc --version | grep 1.81.0 || (echo "Run: rustup default 1.8210"; exit 1)
+rustc --version | grep 1.81.0 || (echo "Run: rustup default 1.81.0"; exit 1)
 
 dir=$(cd `dirname $0`; pwd -P)
 cd ${dir}/..
