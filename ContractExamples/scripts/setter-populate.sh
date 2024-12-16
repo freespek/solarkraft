d6a62396aa866056109c81f56773784e9a1c5053
Igor Konnov <igor@konnov.phd>
2024-12-16 15:46:56 +0100

    remove unnecessary stor

--- ContractExamples/scripts/setter-populate.sh
+++ ContractExamples/scripts/setter-populate.sh
@@ -38,45 +38,45 @@ stellar contract deploy --wasm target/wasm32-unknown-unknown/release/setter.wasm
       --source $ACCOUNT --network $NET 2>.setter.err | tee >.setter.id
 grep "wasm hash" err | sed 's/.*hash \([0-9a-f]*\)/\1/' > .setter.hash
 
-CONTRACT_HASH=`head -n 1 .setter.id`
+CONTRACT_ADDR=`head -n 1 .setter.id`
 WASM_HASH=`head -n 1 .setter.hash`
-echo "export const SETTER_CONTRACT_HASH = \"$CONTRACT_HASH\"" >> $HARDCODED
+echo "export const SETTER_CONTRACT_ADDR = \"$CONTRACT_ADDR\"" >> $HARDCODED
 echo "export const SETTER_WASM_HASH = \"$WASM_HASH\"" >> $HARDCODED
 
-stellar contract invoke --id ${CONTRACT_HASH} --source $ACCOUNT --network $NET \
+stellar contract invoke --id ${CONTRACT_ADDR} --source $ACCOUNT --network $NET \
       -- set_bool --v true
-stellar contract invoke --id ${CONTRACT_HASH} --source $ACCOUNT --network $NET \
+stellar contract invoke --id ${CONTRACT_ADDR} --source $ACCOUNT --network $NET \
       -- set_u32 --v 42
-stellar contract invoke --id ${CONTRACT_HASH} --source $ACCOUNT --network $NET \
+stellar contract invoke --id ${CONTRACT_ADDR} --source $ACCOUNT --network $NET \
       -- set_i32 --v '-42'
-stellar contract invoke --id ${CONTRACT_HASH} --source $ACCOUNT --network $NET \
+stellar contract invoke --id ${CONTRACT_ADDR} --source $ACCOUNT --network $NET \
       -- set_u64 --v 42
-stellar contract invoke --id ${CONTRACT_HASH} --source $ACCOUNT --network $NET \
+stellar contract invoke --id ${CONTRACT_ADDR} --source $ACCOUNT --network $NET \
       -- set_i64 --v '-42'
-stellar contract invoke --id ${CONTRACT_HASH} --source $ACCOUNT --network $NET \
+stellar contract invoke --id ${CONTRACT_ADDR} --source $ACCOUNT --network $NET \
       -- set_u128 --v 42
-stellar contract invoke --id ${CONTRACT_HASH} --source $ACCOUNT --network $NET \
+stellar contract invoke --id ${CONTRACT_ADDR} --source $ACCOUNT --network $NET \
       -- set_i128 --v '-42'
-stellar contract invoke --id ${CONTRACT_HASH} --source $ACCOUNT --network $NET \
+stellar contract invoke --id ${CONTRACT_ADDR} --source $ACCOUNT --network $NET \
       -- set_sym --v hello
-stellar contract invoke --id ${CONTRACT_HASH} --source $ACCOUNT --network $NET \
+stellar contract invoke --id ${CONTRACT_ADDR} --source $ACCOUNT --network $NET \
       -- set_bytes --v beef
-stellar contract invoke --id ${CONTRACT_HASH} --source $ACCOUNT --network $NET \
+stellar contract invoke --id ${CONTRACT_ADDR} --source $ACCOUNT --network $NET \
       -- set_bytes32 --v beef0123456789beef0123456789beef0123456789ab
-stellar contract invoke --id ${CONTRACT_HASH} --source $ACCOUNT --network $NET \
+stellar contract invoke --id ${CONTRACT_ADDR} --source $ACCOUNT --network $NET \
       -- set_vec --v '[ 1, -2, 3 ]'
-stellar contract invoke --id ${CONTRACT_HASH} --source $ACCOUNT --network $NET \
+stellar contract invoke --id ${CONTRACT_ADDR} --source $ACCOUNT --network $NET \
       -- set_map --v '{ "2": 3, "4": 5 }'
-stellar contract invoke --id ${CONTRACT_HASH} --source $ACCOUNT --network $NET \
+stellar contract invoke --id ${CONTRACT_ADDR} --source $ACCOUNT --network $NET \
       -- set_address --v GDIY6AQQ75WMD4W46EYB7O6UYMHOCGQHLAQGQTKHDX4J2DYQCHVCR4W4
-stellar contract invoke --id ${CONTRACT_HASH} --source $ACCOUNT --network $NET \
+stellar contract invoke --id ${CONTRACT_ADDR} --source $ACCOUNT --network $NET \
       -- set_struct --v '{ "a": 1, "b": "-100" }'
-stellar contract invoke --id ${CONTRACT_HASH} --source $ACCOUNT --network $NET \
+stellar contract invoke --id ${CONTRACT_ADDR} --source $ACCOUNT --network $NET \
       -- set_enum --v '{ "B": "-200" }'
-stellar contract invoke --id ${CONTRACT_HASH} --source $ACCOUNT --network $NET \
+stellar contract invoke --id ${CONTRACT_ADDR} --source $ACCOUNT --network $NET \
       -- remove_bool
 
 # NOTE: we do not do that in the CI script
 # we can provoke a failed transaction by submitting 2 transactions in parallel from different accounts
-#stellar contract invoke --id ${CONTRACT_HASH} --source $ACCOUNT --network $NET -- set_bool_if_notset &
-#stellar contract invoke --id ${CONTRACT_HASH} --source $ACCOUNT2 --network $NET -- set_bool_if_notset
+#stellar contract invoke --id ${CONTRACT_ADDR} --source $ACCOUNT --network $NET -- set_bool_if_notset &
+#stellar contract invoke --id ${CONTRACT_ADDR} --source $ACCOUNT2 --network $NET -- set_bool_if_notset
