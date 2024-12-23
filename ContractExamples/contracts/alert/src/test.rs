45a5e31ca4a77d2a0b9e581b325ed43692b8b8e4
Igor Konnov <igor@konnov.phd>
2024-12-23 10:27:48 +0100

    update to the new API and fix the tests in the alert contract

--- ContractExamples/contracts/alert/src/test.rs
+++ ContractExamples/contracts/alert/src/test.rs
@@ -7,33 +7,41 @@ use soroban_sdk::{
 };
 
 #[test]
-fn test() {
+fn test_ok() {
     let env = Env::default();
     let tx_hash: String = String::from_str(&env, "txhash");
 
-    let contract_id = env.register_contract(None, Alert);
+    let contract_id = env.register(Alert, ());
     // <X>Client is automagically created.
     let client = AlertClient::new(&env, &contract_id);
-
 
     assert_eq!(client.emit_and_store_violation(&tx_hash, &VerificationStatus::NoViolation), VerificationStatus::NoViolation);
     // NoViolation triggers an emit but no store
     assert_eq!(
         env.events().all(),
-        vec![&env, (contract_id.clone(),(ALERTS, OK).into_val(&env),VerificationStatus::NoViolation.into_val(&env))]
+        vec![&env, (contract_id.clone(),(ALERTS, OK).into_val(&env), VerificationStatus::NoViolation.into_val(&env))]
     );
 
     // should be empty
     let alerts = client.alerts();
     assert!(alerts.is_empty());
+}
+
+#[test]
+fn test_violation() {
+    let env = Env::default();
+    let tx_hash: String = String::from_str(&env, "txhash");
+
+    let contract_id = env.register(Alert, ());
+    // <X>Client is automagically created.
+    let client = AlertClient::new(&env, &contract_id);
 
     // Violation triggers an emit and a store
     assert_eq!(client.emit_and_store_violation(&tx_hash, &VerificationStatus::Violation), VerificationStatus::Violation);
     assert_eq!(
         env.events().all(),
         vec![&env, 
-            (contract_id.clone(),(ALERTS, OK).into_val(&env),VerificationStatus::NoViolation.into_val(&env)),
-            (contract_id.clone(),(ALERTS, VIOLATION).into_val(&env),VerificationStatus::Violation.into_val(&env))
+            (contract_id.clone(),(ALERTS, VIOLATION).into_val(&env), VerificationStatus::Violation.into_val(&env))
         ]
     );
 
